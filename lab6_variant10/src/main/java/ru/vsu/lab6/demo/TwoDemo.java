d5ccc8ecd435b195d6bdc9d3079f3ba5b9d095a6
boobaaaa <163669248+boobaaaa@users.noreply.github.com>
2026-01-24 15:02:31 +0500

    Delete lab6_variant10 directory

--- lab6_variant10/src/main/java/ru/vsu/lab6/demo/TwoDemo.java
+++
@@ -1,7 +0,0 @@
-package ru.vsu.lab6.demo;
-
-import ru.vsu.lab6.annotations.Two;
-
-@Two(first = "example", second = 42)
-public class TwoDemo {
-}
--- lab6_variant10/src/main/java/ru/vsu/lab6/demo/ValidateDemo.java
+++
@@ -1,7 +0,0 @@
-package ru.vsu.lab6.demo;
-
-import ru.vsu.lab6.annotations.Validate;
-
-@Validate({String.class, Integer.class})
-public class ValidateDemo {
-}
